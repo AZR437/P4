736024c3f23aa6ece90378b72465de67830c433e
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-03-31 21:23:33 +0800

    just removed some comments

--- P4/SceneServer.cpp
+++ P4/SceneServer.cpp
@@ -9,7 +9,6 @@
 #include <fstream>
 SceneServer::SceneServer()
 {
-	this->threadPool = new ThreadPool("ThreadPool", 8);
 }
 void SceneServer::run()
 {
@@ -47,15 +46,13 @@ void SceneServer::RunServer(uint16_t port)
 	grpc::EnableDefaultHealthCheckService(true);
 	grpc::reflection::InitProtoReflectionServerBuilderPlugin();
 	grpc::ServerBuilder builder;
-	// Listen on the given address without any authentication mechanism.
+
 	builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
-	// Register "service" as the instance through which we'll communicate with
-	// clients. In this case it corresponds to an *synchronous* service.
+
 	builder.RegisterService(&service);
 	// Finally assemble the server.
 	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
 	std::cout << "Server listening on " << serverAddress << std::endl;
-	// Wait for the server to shutdown. Note that some other thread must be
-	// responsible for shutting down the server for this call to ever return.
+
 	server->Wait();
 }
