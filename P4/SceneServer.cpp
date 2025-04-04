d889de12878ce20e2a9274bed9decee94bc949de
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-04-04 11:58:52 +0800

    Fix

--- P4/SceneServer.cpp
+++ P4/SceneServer.cpp
@@ -17,7 +17,7 @@ void signalHandler(int signum) {
 
 grpc::Status MeshStreamImpl::StreamMesh(grpc::ServerContext* context, const MeshRequest* request, grpc::ServerWriter<MeshReply>* writer)
 {
-	std::string path = "C:/Users/theas/Documents/Acads/P4/P4/Assets/" + request->file_name() + ".obj";
+	std::string path = "Assets/" + request->file_name() + ".obj";
 	std::ifstream file(path, std::ios::binary);
 	if (!file.is_open()) {
 		return grpc::Status(grpc::StatusCode::NOT_FOUND, "File not found");
@@ -79,14 +79,14 @@ bool SceneLoadImpl::retryLoadSceneFromFile(const std::string& scene_id, std::vec
 		if (loadSceneFromFile(scene_id, meshIDs, positions, scales)) {
 			return true;
 		}
-		std::cerr << "Retrying to load scene file, attempt " << i + 1 << " of " << retries << ".../n";
+		std::cerr << "Retrying to load scene file, attempt " << i + 1 << " of " << retries << "...\n";
 		this->server_->sleep(1000);
 	}
 	return false;
 }
 bool SceneLoadImpl::loadSceneFromFile(const std::string& scene_id, std::vector<std::string>& meshIDs, std::vector<Position>& positions, std::vector<Scale>& scales)
 {
-	std::string path = "C:/Users/theas/Documents/Acads/P4/P4/Scenes/" + scene_id + ".json";
+	std::string path = "Scenes/" + scene_id + ".json";
 	std::ifstream file(path, std::ios::in);
 	if (!file.is_open()) {
 		std::cerr << "Failed to open scene file: " << path << std::endl;
---
+++ README.txt
@@ -0,0 +1,5 @@
+1. To Run and work File first you must build the server to do this go to main then change the #define to Server before building.
+2. Before running the server copy and paste the following folders into the area where the P4.exe is(Assets, Scenes, Shaders, Skybox).
+3. After doing this rename the currently made P4 into P4_Server.
+4. Run P4_server.
+5. Make the Client to do this set the define of main to Client the build using the local windows debugger.
